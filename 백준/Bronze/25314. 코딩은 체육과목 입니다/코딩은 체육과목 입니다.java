5678a799bfce11739b3caef4ef76e08716136869
Dragon Ball <yzon007@naver.com>
2024-07-19 14:41:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 212 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N;
+        N = sc.nextInt();
+
+        for (int i = 1; i <= (N/4); i++) {
+            System.out.print("long ");
+        }
+            System.out.print("int");
+    }
+}
