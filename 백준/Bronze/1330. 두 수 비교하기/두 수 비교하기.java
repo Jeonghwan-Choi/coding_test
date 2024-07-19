f73ae010de5d36adb900ef8958318a3719451201
Dragon Ball <yzon007@naver.com>
2024-07-19 12:08:07 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long A, B, C;
+
+        A = Long.parseLong(sc.next());
+        B = Long.parseLong(sc.next());
+
+        System.out.println(A > B ? ">" : A == B ? "==" : "<");
+    }
+
+}
